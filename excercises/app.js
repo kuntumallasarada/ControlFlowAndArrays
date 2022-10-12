81251dade12f910372d697850cc90c88a19bd0ab
Sarada Nookala <kuntumallasarada@gmail.com>
2022-10-12 15:34:51 -0600

    more if/else statements

--- excercises/app.js
+++ excercises/app.js
@@ -11,3 +11,32 @@ if(num<0)
 }
 }
 test(10);
+
+var num1=20;
+if(num1>0)
+{
+if(num1>100)
+{
+    console.log( "number is positive and greater than 100");
+}
+else if(num1<100)
+{
+    console.log("number is positive and less than 100");
+}
+}
+else if(num1<0)
+    {
+    console.log( "number is negative");
+    }
+
+    var marks=7;
+    if(marks>=90)
+    console.log("Grade A");
+    if(marks>=80 && marks<90)
+    console.log("Grade B");
+    if(marks>=70 && marks<80)
+    console.log("Grade C");
+    if(marks>=55 && marks<70)
+    console.log("Grade D");
+    if(marks<55)
+    console.log("Grade F");
