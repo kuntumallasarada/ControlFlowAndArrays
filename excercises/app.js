43526db1ad1e2be7d587a11fd390ba7732776928
Sarada Nookala <kuntumallasarada@gmail.com>
2022-10-12 15:01:23 -0600

    if and else statements

--- excercises/app.js
+++ excercises/app.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+var num;
+function test(num){
+if(num>=0)
+{
+    console.log( "num is positive");
+}
+if(num<0)
+{
+    console.log("num is negative");
+}
+}
+test(10);
